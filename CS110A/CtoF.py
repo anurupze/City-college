a68bf8a252322aaeb2060f789d09d27a4797c1d7
Anurup <anurupbro@gmail.com>
2023-01-24 22:10:18 -0800

    Celsius to Fahrenheit equivalent values.

--- CS110A/CtoF.py
+++ CS110A/CtoF.py
@@ -14,10 +14,17 @@ print("\t _ _ _ _ _ _ _ _ _ _ _ _ \n ")
 
 # Using a for loop to iterate through the range of the temprature
 for c in range(-20, 19, 2):
+
     # Calculation to print the fahrenheit values.
     f = 9 * c / 5 + 32
+
     # Print statement with required decimal count after the number.
-    print(" %13.0F %13.1F"  %(c , f))
+    print(" %13.0F %13.1F "  %(c , f))
 
 
 print("\t _ _ _ _ _ _ _ _ _ _ _ _ \n ")
+
+'''
+The print statement with modulus are the format specifiers.
+This program doesnot take any input and outputs the fahrenheit equivalents of celsius readings.
+'''
