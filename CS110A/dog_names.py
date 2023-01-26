7318429cbfe7d7d15a5afbd73b8cd68cc37630ac
Anurup <anurupbro@gmail.com>
2023-01-25 22:36:54 -0800

    initial phase of the code contains pseudocode

---
+++ CS110A/dog_names.py
@@ -0,0 +1,13 @@
+''' 
+This program allows a user to input a dogs name to see if it is one of the popular names.
+Inputs : name of dog
+Outputs : Is it a popular dogs name
+written by : Anurup 
+Written date : 01/25/2023
+'''
+
+# Create a file where we input popular dog names.
+
+# Open the previously created file in read mode.
+
+# Take user input
